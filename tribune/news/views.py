7f303db1d9659800d3a57e412ccc40408243d4b9
cheruden <cheruden25@gmail.com>
2021-06-30 14:48:16 +0300

    add display functionality

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
-
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()
-    return render(request, 'all-news/today-news.html',{'date': date})
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html',{'date': date,"news":news})
 
 
 
@@ -23,6 +22,20 @@ def past_days_news(request,past_date):
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
+    
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date":date,"news":news})
 
-    return render(request, 'all-news/past-news.html', {"date":date})
+def search_results(request):
+
+         if 'article' in request.GET and request.GET["article"]:
+             search_term = request.GET.get("article")
+             searched_articles = Article.search_by_title(search_term)
+             message = f"{search_term}"
+
+             return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+         else:
+           message = "You haven't searched for any term"
+           return render(request, 'all-news/search.html',{"message":message})
