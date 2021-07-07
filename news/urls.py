efeae43d500d83c3300a2f29c98ff70efd675cb2
cheruden <cheruden25@gmail.com>
2021-07-07 22:18:30 +0300

    make responsive

---
+++ news/urls.py
@@ -0,0 +1,39 @@
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.conf.urls import url
+# from . import views
+# from django.contrib.auth import views as display 
+
+
+# urlpatterns=[
+    
+#     url(r'^$', views.news_today, name='newsToday'),
+#     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
+#     url(r'^search/', views.search_results, name='search_results'),
+#     url(r'^article/(\d+)', views.new_article, name='article'),
+#     url(r'^logout/$', display.LogoutView, {"next_page": '/'}),
+#     url(r'^new/article$', views.new_article, name='new-article'),
+# ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.news_today, name='newsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',
+        views.past_days_news, name='pastNews'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^article/(\d+)', views.article, name='article'),
+    url('new/article', views.new_article, name='new-article'),
+    url(r'^accounts/profile', views.profile, name='user-profile'),
+
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
